6325e69f3098849cd85b1d208fa1fe2a01a7487b
Mister Gillouz <34444967+Mistergillouz@users.noreply.github.com>
2020-01-04 07:02:00 +0100

    ++

---
+++ src/Workbench.function.js
@@ -0,0 +1,36 @@
+WorkbenchController.prototype._dumpStore = function () {
+  // eslint-disable-next-line quotes
+  const storesList = ["documentStore", "workbenchStore", "feedbackStore", "reportElementStore", "reportStore", "dataObjectStore", "dataProviderStore", "documentInputControlStore", "reportInputControlStore", "workbenchElementStore", "configurationStore", "viewStore", "alerterStore", "commentStore", "documentInfosStore", "documentOpendocStore", "drillerFilterStore", "logonStore", "pageStore", "sessionStore", "sortStore", "variantStore", "webAppInfoStore", "webiStore", "appStore", "elementLinkStore", "featureToggleStore", "filterStore", "fullScreenStore", "wiseAppStore", "wiseViewStore", "workbenchErrorStore", "workbenchFilterBarStore", "workbenchSnapshotStore", "workbenchWarningStore" ]
+  const stores = {}
+  storesList.forEach((name) => {
+    const storeName = name.charAt(0).toUpperCase() + name.substring(1)
+    const store = StoreRegistry[`get${storeName}`]()
+    if (store instanceof PropertiesAbstractStore) {
+      Object.keys(store).forEach((storeKey) => {
+        if (typeof store[storeKey] === 'function' && storeKey.startsWith('get') && !storeKey.endsWith('Key')) {
+          if (!stores[storeName]) {
+            stores[storeName] = []
+          }
+          stores[storeName].push(storeKey)
+        }
+      })
+    } else {
+      const data = store.getData('/')
+      stores[storeName] = Object.keys(data).map((key) => key)
+    }
+
+    if (stores[storeName].length) {
+      stores[storeName].sort()
+      stores[storeName] = `|${stores[storeName].join(',')}|`
+    } else {
+      delete stores[storeName]
+    }
+  })
+
+  const helpers = Object.values(HelperRegistry.getObjects()).map((object) => object.getType())
+  console.log(JSON.stringify({
+    stores,
+    helpers,
+    actions: Object.keys(ActionRegistry).filter((key) => key === key.toUpperCase())
+  }))
+}
